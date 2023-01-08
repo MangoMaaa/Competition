977b4712e96309dc386cc98f25ef10223db5fb49
TheMalevolentOne1 <kylerobinson818@gmail.com>
2023-01-08 01:49:05 +0000

    backend update

--- backend.py
+++ backend.py
@@ -1,20 +1,32 @@
 import sqlite3
 
-# shows all of the competitors
+# Connects to the database :)
 db = sqlite3.connect("dance_competition.db")
 
 cur = db.cursor()
 
+#Competitor Functions:
+
 def view_all_competitors():
     return cur.execute("SELECT * FROM tblCompetitors")
+
+def competitor_search(*array):
+    return "Incomplete"
+
+# Activity Functions:
 
 def activities_view_all():
     # shows all of the activities
     return cur.execute("SELECT * FROM tblActivity")
 
+# Event Functions:
+
 def events_view_all():
     return cur.execute("SELECT * FROM tblEvents INNER JOIN tblCompetitors on tblCompetitors.CompetitorID = tblEvents.CompetitorID;")
 
 def event_search(*array):
-    print(array)
     return cur.execute("")
+
+#Leaderboard Functions:
+
+#Admin Functions:
